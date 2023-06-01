f8c90f9b8ebdf437fcfe933d5cb3c99cfb696432
Nikita Shadrin <shadrinnikita@gmail.com>
2023-06-01 06:00:10 +0300

    Fixed props issue

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
-import type { PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
 
-export const PageLayout = (props: PropsWithChildren<{}>) => {
+export const PageLayout = (props: PropsWithChildren) => {
   return (
       <main className="h-screen flex justify-center">
         <div className="w-full md:max-w-2xl border-x h-full border-slate-400 ">
